042343d08a1af64ca7d07cb6a801f6f2d11f2dc0
Kaushal M <kaushal.madappa@dominodatalab.com>
2020-10-16 17:17:27 +0530

    Switch to packager

--- lua/init.lua
+++ lua/init.lua
@@ -2,24 +2,9 @@ local vim = vim
 local api = vim.api
 
 -- lsp-status settings
-local lsp_status = require('lsp-status')
-
 local kind_labels_mt = {__index = function(_, k) return k end}
 local kind_labels = {}
 setmetatable(kind_labels, kind_labels_mt)
-
-lsp_status.register_progress()
-lsp_status.config({
-  kind_labels = kind_labels,
-  indicator_errors = "×",
-  indicator_warnings = "!",
-  indicator_info = "i",
-  indicator_hint = "›",
-  -- the default is a wide codepoint which breaks absolute and relative
-  -- line counts if placed before airline's Z section
-  status_symbol = "",
-})
-
 
 -- nvim-lsp settings
 local nvim_lsp = require('nvim_lsp')
@@ -28,7 +13,6 @@ local configs = require('nvim_lsp/configs')
 local on_attach = function(client, bufnr)
   require'completion'.on_attach(client, bufnr)
   require'diagnostic'.on_attach(client, bufnr)
-  lsp_status.on_attach(client)
 
   local opts = {noremap = true, silent = true}
   -- cf. :help lsp-config for tips on what to map
@@ -63,31 +47,9 @@ for ls, settings in pairs(servers) do
   nvim_lsp[ls].setup{
     on_attach = on_attach,
     settings = settings,
-    capabilities = vim.tbl_extend("keep", configs[ls].capabilities or {}, lsp_status.capabilities)
   }
 end
 
---nvim_lsp.bashls.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.vimls.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.gopls.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.pyls.setup{
-  --settings = {
-    --pyls = {
-      --configurationSources = { "pycodestyle", "pyflakes" }
-    --}
-  --},
-  --on_attach = on_attach,
-  --capabilities = lsp_status.capabilities
---}
---nvim_lsp.rust_analyzer.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.jsonls.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.yamlls.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.metals.setup{ on_attach = on_attach, capabilities = lsp_status.capabilities }
---nvim_lsp.sumneko_lua.setup{
-  --cmd = { "/Users/kaushal/.cache/nvim/nvim_lsp/sumneko_lua/lua-language-server/bin/macOS/lua-language-server", "-E", "/Users/kaushal/.cache/nvim/nvim_lsp/sumneko_lua/lua-language-server/main.lua" },
-  --on_attach = on_attach,
-  --capabilities = lsp_status.capabilities
---}
 
 -- nvim-treesitter settings
 require'nvim-treesitter.configs'.setup {
