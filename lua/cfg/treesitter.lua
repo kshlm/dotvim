d07f6ec5085d2b8f385593f483a49f597781027b
Kaushal M <kshlmster@gmail.com>
2021-02-08 14:33:45 +0530

    Fix config when loading plugins

--- lua/cfg/treesitter.lua
+++ lua/cfg/treesitter.lua
@@ -1,14 +1,12 @@
-return function()
-  -- nvim-treesitter settings
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = 'maintained',     -- one of 'all', 'language', or a list of languages
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    refactor = {
-      highlight_definitions = { enable = true },
-      smart_rename = { enable = true },
-      navigation = { enable = true },
-    },
-    textobjects = { enable = true },
-  }
-end
+-- nvim-treesitter settings
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = 'maintained',     -- one of 'all', 'language', or a list of languages
+  highlight = { enable = true },
+  incremental_selection = { enable = true },
+  refactor = {
+    highlight_definitions = { enable = true },
+    smart_rename = { enable = true },
+    navigation = { enable = true },
+  },
+  textobjects = { enable = true },
+}
--- lua/init.lua
+++ lua/init.lua
@@ -2,10 +2,6 @@ local M = {}
 
 function M.setup()
   pcall(require 'plugins')
-  pcall(require 'cfg.lsp')
-  pcall(require 'cfg.treesitter')
-  pcall(require 'cfg.telescope')
-  pcall(require 'cfg.nvimtree')
 end
 
 return M
